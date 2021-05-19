5d100e7cbd4040960038ad49b660ef8da27489f2
wanty <wantydream@gmail.com>
2021-05-19 02:04:01 +0200

    stuff

--- lib/core/argparse.py
+++ lib/core/argparse.py
@@ -2,7 +2,9 @@
 from argparse import ArgumentParser
 import sys
 # LPL - Local Python Libraries
+from lib.core.attr import attr_wpnList
 from lib.core.enums import WPN
+from lib.core.log import logger
 
 
 def argParser(argv=None):
@@ -29,24 +31,67 @@ def argParser(argv=None):
 
 
 def argAction(args, parser):
-    pass
+    logger.info(vars(args))  # Log the command arguments used
+    if not args.command:  # Display help message if parser is not used
+        parser.parse_args(["--help"])
+        sys.exit(0)
+    if not args.rootDirectory:  # Display help message if no rootDirectory
+        logger.info("No root directory defined")
+        parser.parse_args(["--help"])
+        sys.exit(0)
+    if args.command == "wpn":
+        argSubaction_wpn(args, parser)
 
 
 def subparser_wpn(subparser):
     parser_wpn = subparser.add_parser("wpn", help="Weapons")
     parser_wpn.set_defaults(parser_wpn=True)
+    wpn_enums = attr_wpnList()
 
     wpnMDL = parser_wpn.add_argument_group(
         "3D Models",
         "Commands for weapon 3D models (.mdl) file manipulation"
     )
     wpnMDL.add_argument(
-        "--wpnConvertMDL1P",
-        dest="wpnConvertMDL1P",
+        "--wpn-convertMDL",
+        dest="wpnConvertMDL",
+        action="store_true",
+        help="Convert a weapon mdl to a defined version"
+    )
+    wpnMDL.add_argument(
+        "--wpn-fileType",
+        dest="wpnFileType",
+        choices=[
+            WPN.TYPE_1P,
+            WPN.TYPE_3P,
+            WPN.TYPE_HP,
+            WPN.TYPE_MP
+        ],
+        help="Choose the 3D model file type (1P, 3P, etc.)"
+    )
+    wpnMDL.add_argument(
+        "--wpn-fileVersion",
+        dest="wpnFileVersion",
         choices=[
             WPN.VERSION_V1,
             WPN.VERSION_V2,
             WPN.VERSION_VANILLA,
         ],
-        help="Set the extracted VPK directory"
+        help="Choose the 3D model file version"
     )
+    wpnMDL.add_argument(
+        "--wpn-fileTarget",
+        dest="wpnFileTarget",
+        choices=wpn_enums,
+        help="Choose the 3D model target file"
+    )
+    wpnMDL.add_argument(
+        "--wpn-structTarget",
+        dest="wpnstructTarget",
+        choices=wpn_enums,
+        help="Choose the 3D model struct target"
+    )
+
+
+def argSubaction_wpn(args, parser):
+    pass
---
+++ lib/core/log.py
@@ -0,0 +1,35 @@
+# SPL - Standard Python Libraries
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+import sys
+
+
+def getLogger(loggerName):
+    loggerDir = "logs"
+    loggerFile = "{0}\\log".format(loggerDir)
+
+    # Create log folder if it does not exist
+    if not os.path.exists(loggerDir):
+        os.makedirs(loggerDir)
+
+    # 00:00:42 | WPN | [INFO] | Done super secret modding method
+    loggingFormatter = logging.Formatter(
+        "%(asctime)s | [%(levelname)s] | %(message)s",
+        "%H:%M:%S"
+    )
+
+    consoleHandler = logging.StreamHandler(sys.stdout)
+    consoleHandler.setFormatter(loggingFormatter)
+    fileHandler = TimedRotatingFileHandler(loggerFile, when="midnight")
+    fileHandler.setFormatter(loggingFormatter)
+
+    logger = logging.getLogger(loggerName)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(consoleHandler)
+    logger.addHandler(fileHandler)
+    logger.propagate = False
+    return(logger)
+
+
+logger = getLogger("logger")
